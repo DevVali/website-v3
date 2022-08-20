d17a6c013fdc8b57e7b1d7e9551a6939f183b064
DevVali <90271108+DevVali@users.noreply.github.com>
2022-08-20 19:35:38 +0000

    change countapi.xyz key

--- includeto/pagefooter.php
+++ includeto/pagefooter.php
@@ -3,7 +3,7 @@
   <a href="https://www.instagram.com/<?php echo $username ?>"><i class="fa-brands fa-instagram"></i></a>&nbsp;&nbsp;&nbsp;
   <a href="https://www.twitter.com/<?php echo $username ?>"><i class="fa-brands fa-twitter"></i></a>&nbsp;&nbsp;&nbsp;
   <p>This site was made by hand with the use of <a href="https://github.com/DevVali/Boostyle">Boostyle</a>, <a href="https://getbootstrap.com">Bootstrap</a> and <a href="https://www.w3schools.com/w3css/">W3.css</a>. <a href="https://github.com/DevVali/ejvali-eu">Get the source code.</a><br>This site has been visited <span id="visits"><code>undefinied</code></span> times.</p>
-  <script async src="https://api.countapi.xyz/hit/ejvali-eu/7d9c6b69-0357-4207-91ff-fbf5fc266d8f?callback=websiteVisits"></script>
+  <script async src="https://api.countapi.xyz/hit/ejvali-eu/810c7fcc-b244-4bae-84f9-548a229b3b82?callback=websiteVisits"></script>
   <script>
     function websiteVisits(response) {
       document.querySelector("#visits").textContent = response.value;
